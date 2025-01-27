eeba417a76d1381094140f43e5bf8b8ce68a38fb
RamadanismaeL <ramadan.ismael02@gmail.com>
2025-01-27 14:00:18 +0200

    initial commit

---
+++ src/app/_components/magic-card/magic-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-magic-card',
+  imports: [],
+  templateUrl: './magic-card.component.html',
+  styleUrl: './magic-card.component.scss'
+})
+export class MagicCardComponent {
+
+}
